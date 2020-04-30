0c4595d33eca9912ecfe5e653479e7f7939048ab
chingaloEric <echingalo@gmail.com>
2020-04-30 20:26:59 +0300

    feat: 🎸 registered the app effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from './store/reducers/';
+import { loadCountryData } from './store/actions/country-data.actions';
+import { loadWorldData } from './store/actions/world-data.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'corona-dashboard';
+export class AppComponent implements OnInit {
+  constructor(private store: Store<State>) {}
+
+  ngOnInit() {
+    this.store.dispatch(loadCountryData());
+    this.store.dispatch(loadWorldData());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { reducers, metaReducers } from './store/reducers';
+import { effects } from './store/effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +21,7 @@ import { reducers, metaReducers } from './store/reducers';
     BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot(effects),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
