09cbf1cb8a810cc60f95c9a73755d3b377878fc9
chingaloEric <echingalo@gmail.com>
2020-04-30 16:43:53 +0300

    feat: 🎸 added the get methods for that data
    used the api endpoints given by https://covid19api.com

--- src/app/home/services/covid.service.ts
+++ src/app/home/services/covid.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CovidService {
+  country = 'tanzania';
   constructor(private httpClient: HttpClient) {}
+
+  getCountryData(): Observable<any> {
+    return this.httpClient.get(
+      `https://api.covid19api.com/country/${this.country}`
+    );
+  }
+
+  getWorldData(): Observable<any> {
+    return this.httpClient.get('https://api.covid19api.com/world/total');
+  }
 }
