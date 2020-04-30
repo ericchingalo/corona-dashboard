d78019294a523edf4b43b894324e38b955d8bddc
chingaloEric <echingalo@gmail.com>
2020-04-30 20:41:49 +0300

    feat: 🎸 added a data sanitizer for the country data
    the sanitizer method convers the api response to usefull informattion

--- src/app/core/helpers/country-data-sanitizer.helper.ts
+++ src/app/core/helpers/country-data-sanitizer.helper.ts
@@ -1,1 +1,19 @@
-export function countryDataSanitizer() {}
+import * as _ from 'lodash';
+import { generateId } from './id-generator.helper';
+
+export function countryDataSanitizer(dataArray: any[]) {
+  return _.forEach(dataArray, (data: any) =>
+    Object.assign(
+      {},
+      {
+        id: generateId(),
+        country: data.Country,
+        date: data.Date,
+        confirmed: data.Confirmed,
+        deaths: data.Deaths,
+        recovered: data.Recovered,
+        active: data.Confirmed - (data.Recovered + data.Deaths),
+      }
+    )
+  );
+}
