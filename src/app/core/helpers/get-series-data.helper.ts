96c5503dfb6fd75adf40fc5e21425c4b41b8e72d
chingaloEric <echingalo@gmail.com>
2020-05-01 19:30:41 +0300

    feat: 🎸 added helper method to get series data for the charts

--- src/app/core/helpers/get-series-data.helper.ts
+++ src/app/core/helpers/get-series-data.helper.ts
@@ -2,10 +2,52 @@ import * as _ from 'lodash';
 import { CountryData } from '../models/country-data.model';
 
 export function getConfirmedData(data: CountryData[]) {
-  const seriesData: number[] = [];
+  const confirmedData: number[] = [];
   _.forEach(data, (countryData: CountryData) =>
-    seriesData.push(countryData.confirmed)
+    confirmedData.push(countryData.confirmed)
   );
 
-  return seriesData;
+  return confirmedData;
+}
+
+export function getRecoveredData(data: CountryData[]) {
+  const recoveredData: number[] = [];
+  _.forEach(data, (countryData: CountryData) =>
+    recoveredData.push(countryData.recovered)
+  );
+
+  return recoveredData;
+}
+
+export function getDeathData(data: CountryData[]) {
+  const deathData: number[] = [];
+  _.forEach(data, (countryData: CountryData) =>
+    deathData.push(countryData.deaths)
+  );
+
+  return deathData;
+}
+
+export function getActiveData(data: CountryData[]) {
+  const activeData: number[] = [];
+  _.forEach(data, (countryData: CountryData) =>
+    activeData.push(countryData.active)
+  );
+
+  return activeData;
+}
+
+export function getNewCasesData(data: CountryData[]) {
+  const newCaseData: number[] = [];
+  let count = -1;
+  _.forEach(data, (countryData: CountryData) => {
+    if (data[0] === countryData) {
+      newCaseData.push(countryData.confirmed);
+    } else {
+      newCaseData.push(countryData.confirmed - data[count].confirmed);
+    }
+    count++;
+  });
+
+  return newCaseData;
 }
