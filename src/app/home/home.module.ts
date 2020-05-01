b34e669d0a96a37308104e37d12672e8d8c65281
chingaloEric <echingalo@gmail.com>
2020-05-01 03:59:22 +0300

    feat: 🎸 added material module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { components } from './components';
+import { MaterialModule } from '../core/material/material.module';
 
 @NgModule({
   declarations: [...components],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
 })
 export class HomeModule {}
